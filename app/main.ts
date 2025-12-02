6f424b8f74c57283a54b602757b781c410a76091
Alex <steblyakov.aleksandr@gmail.com>
2025-12-03 00:13:45 +0300

    step 5 of the challenge - adding echo output

--- app/main.ts
+++ app/main.ts
@@ -5,17 +5,32 @@ const rl = createInterface({
   output: process.stdout,
 });
 
+function SplitInputCommand(commandInput: string) :string[] {
+    const index = commandInput.trim().indexOf(' ');
+    if (index !== -1)
+    {
+        return [commandInput.trim().slice(0, index), commandInput.trim().slice(index + 1)];
+    }
+
+    return [commandInput.trim()];
+}
+
 function run() {
 
     rl.question('$ ', (command) => {
 
-        if(command === 'exit') {
+        if(command.trim() === 'exit') {
             rl.close();
             // console.log("Exited...");
             return;
         }
 
-        if(true)
+        const splitCommand = SplitInputCommand(command);
+
+        if (splitCommand.length > 1 && splitCommand[0] === 'echo') {
+            console.log(splitCommand[1]);
+        }
+        else
         {
             console.log(`${command}: command not found`);
         }
