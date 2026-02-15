32061375790605170a4448e0a47fc74d67ce74f6
Alex <steblyakov.aleksandr@gmail.com>
2026-02-15 17:32:18 +0300

    Backslash within single quotes #le5  - 3rd

--- app/main.ts
+++ app/main.ts
@@ -62,7 +62,7 @@ async function run() {
 
         } else if (splitCommand.length > 1 && splitCommand[0] === AvaliableCommands.echo) {
             const commandsToPrint = splitCommand.slice(1);
-            console.log(commandsToPrint.join(''));
+            console.log(commandsToPrint.join(' '));
         } else if (splitCommand.length > 1 && splitCommand[0] === AvaliableCommands.type) {
             if (splitCommand[1] in AvaliableCommands) {
                 console.log(`${splitCommand[1]} is a shell builtin`);
