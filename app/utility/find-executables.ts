54c3ce6eb20d6bbbdcdd9e81c1a463ad2d37aef6
Alex <steblyakov.aleksandr@gmail.com>
2026-02-17 17:28:44 +0300

    Executing a quoted executable #qj0 - 9th attempt

--- app/utility/find-executables.ts
+++ app/utility/find-executables.ts
@@ -1,32 +1,46 @@
 import {join} from "path";
-import {constants, stat} from "fs";
+import {access, stat} from "fs";
 import {promisify} from "util";
 import {PathManager} from "./path-manager.ts";
 import * as path from "node:path";
 
+import { constants } from 'fs/promises';
 
-const statPromise = promisify(stat);
+
+// const statPromise = promisify(stat);
+//
+// async function isFileExecutable(filePath: string): Promise<boolean> {
+//     try {
+//         const stats = await statPromconstantsise(filePath);
+//
+//         if (!stats.isFile()) {
+//             return false;
+//         }
+//
+//         return true;
+//         // const mode = stats.mode;
+//         // return (
+//         //     (mode & constants.S_IXUSR) !== 0 || // Для владельца
+//         //     (mode & constants.S_IXGRP) !== 0 || // Для группы
+//         //     (mode & constants.S_IXOTH) !== 0    // Для других
+//         // );
+//     }
+//     catch {
+//         return false;
+//     }
+// }
+
+const accessPromise = promisify(access);
 
 async function isFileExecutable(filePath: string): Promise<boolean> {
     try {
-        const stats = await statPromise(filePath);
-
-        if (!stats.isFile()) {
-            return false;
-        }
-
+        await accessPromise(filePath, constants.X_OK);
         return true;
-        // const mode = stats.mode;
-        // return (
-        //     (mode & constants.S_IXUSR) !== 0 || // Для владельца
-        //     (mode & constants.S_IXGRP) !== 0 || // Для группы
-        //     (mode & constants.S_IXOTH) !== 0    // Для других
-        // );
-    }
-    catch {
+    } catch {
         return false;
     }
 }
+
 
 // function getWindowsExecutablePaths(fileName: string): string[] {
 //     const extensions = (process.env.PATHEXT || '').split(';');
@@ -52,17 +66,16 @@ export async function findExecutableInPath(fileName: string): Promise<string | n
 
     // здесь теоретически должна быть проверка на то содержится ли допустимое
     // расширение файла для Windows в переданном названии файла 'fileString'
-    console.log("COMMAND NAME RECIEVED:", fileName);
+    // console.log("COMMAND NAME RECIEVED:", fileName);
 
     for (const dir of pathDirectories) {
         // const fullPath = join(dir, "\"" + fileName + "\"");
         // const fullPath = join(dir, fileName);
         const fullPath = path.resolve(dir, fileName);
-        console.log("FULL PATH TESTED:", fullPath);
+        // console.log("FULL PATH TESTED:", fullPath);
 
         if (await isFileExecutable(fullPath)) {
             return fullPath;
-            //    }
         }
     }
 
